5b578db6c8f703f2c23cc62814eafe2be586887c
mojaeya <mojaeya@gmail.com>
2023-03-08 17:50:22 +0900

    seconde commit

---
+++ vue/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p></p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: `John Doe`
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
